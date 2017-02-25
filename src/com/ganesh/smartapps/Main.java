f9b397b84bc2f03f8c5b060c0303e1e867a95f71
pavant2007 <pavant2007@gmail.com>
2017-02-25 20:15:54 +0300

    ganesh

--- src/com/ganesh/smartapps/Main.java
+++ src/com/ganesh/smartapps/Main.java
@@ -1,9 +1,30 @@
 package com.ganesh.smartapps;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Welcome to Ganesh Software Solutions!");
+
+        Date date = new Date();
+        GregorianCalendar gc = new GregorianCalendar();
+        gc.setTime(date);
+
+        DatatypeFactory dc = null;
+        try {
+            dc = DatatypeFactory.newInstance();
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
+        if(null != dc) {
+            XMLGregorianCalendar calendar = dc.newXMLGregorianCalendar(gc);
+            System.out.println(calendar);
+        }
     }
 }
