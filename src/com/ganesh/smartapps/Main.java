9ae0bca7190f3e5ce2cd7cae5cfcadf59a5609c5
pavant2007 <pavant2007@gmail.com>
2017-02-25 20:05:47 +0300

    ganesh

--- src/com/ganesh/smartapps/Main.java
+++ src/com/ganesh/smartapps/Main.java
@@ -3,7 +3,7 @@ package com.ganesh.smartapps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code hereccf
+	// write your code hereccf 
 
         System.out.println("Welcome to Ganesh Software Solutions!");
 
